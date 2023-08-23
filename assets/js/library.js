6a1673fbf56b0375b55e5d876f61892f2c5b3c75
Ollie <olliebyrne2@outlook.com>
2023-08-23 20:08:19 +0100

    borrowing books

--- assets/js/library.js
+++ assets/js/library.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const url = "https://book-wiz-jdyf.onrender.com/books";
 
     function createBookElement(book) {
-        const {name, author, year, genre, description, image} = book;
+        const {id, name, author, year, genre, description, image} = book;
 
         const bookDiv = document.createElement('div');
         bookDiv.classList.add('libraryDiv');
@@ -34,11 +34,50 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const btn = document.createElement('button');
         btn.classList.add('borrowBtn');
+        btn.setAttribute('data-book-id', id); 
         btn.textContent = 'Borrow';
+        btn.addEventListener('click', handleBorrow);
         bookDiv.appendChild(btn);
 
         return bookDiv;
     }
+
+    function handleBorrow(event) {
+        const bookId = event.target.getAttribute('data-book-id');
+        
+        const userId = '1';
+    
+        const currentDate = new Date();
+        currentDate.setMonth(currentDate.getMonth() + 1);
+        const dueDate = currentDate.toISOString().slice(0, 19).replace('T', ' ');
+    
+        const data = {
+            book_id: bookId,
+            user_id: userId,
+            due_date: dueDate
+        };
+    
+        const token = localStorage.getItem('token');
+    
+        fetch('https://book-wiz-jdyf.onrender.com/borrowed', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}` 
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert('Book borrowed successfully!');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Sorry, you must be logged in to borrow books.');
+        });
+    }
+    
+    
 
     function addBooks(books) {
         books.forEach(book => {
