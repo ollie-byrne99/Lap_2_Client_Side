b2c05056ba452ef1b0b572468a536106df688fde
Ollie <olliebyrne2@outlook.com>
2023-08-23 11:20:50 +0100

    login changes

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,8 @@
 document.querySelector('.lrBtn').addEventListener('click', async (e) => {
     e.preventDefault();
 
-    const email = document.querySelector('input[placeholder="Email"]').value;
+    
+    const username = document.querySelector('input[placeholder="Username"]').value;
     const password = document.querySelector('input[placeholder="Password"]').value;
 
     const options = {
@@ -11,18 +12,23 @@ document.querySelector('.lrBtn').addEventListener('click', async (e) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            email: email,
+            username: username,  
             password: password
         })
     };
 
-    const response = await fetch("http://localhost:3000/users/login", options);
-    const data = await response.json();
+    try {
+        const response = await fetch("http://localhost:3000/users/login", options);
+        const data = await response.json();
 
-    if (response.status == 200) {
-        localStorage.setItem("token", data.token);
-        window.location.assign("./library.html");
-    } else {
-        alert(data.error);
+        if (response.status == 200) {
+            localStorage.setItem("token", data.token);
+            window.location.assign("./library.html");
+        } else {
+            alert(data.error);
+        }
+    } catch (error) {
+        console.error("Error during login:", error);
+        alert("Failed to login. Please try again.");
     }
 });
